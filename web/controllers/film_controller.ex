e4d3e9651f2ae1d67d9cb06db254100d5e090703
James Tortorelli <onetoedsloth@icloud.com>
2016-10-23 14:21:01 -0500

    Add list of films to film index page

--- web/controllers/film_controller.ex
+++ web/controllers/film_controller.ex
@@ -1,8 +1,10 @@
 defmodule Cineaste.FilmController do
   use Cineaste.Web, :controller
+  alias Cineaste.FilmIndexView
 
   def index(conn, _params) do
-    render conn, "index.html"
+    film_index_views = Repo.all(Cineaste.FilmIndexView) |> Enum.sort_by(fn(view) -> FilmIndexView.sort_title(view) end) 
+    render conn, "index.html", film_index_views: film_index_views
   end
 
   def show(conn, %{"id" => id}) do
