c6303fed8ebb2eeeac29f78468d99988f24781a8
James Tortorelli <onetoedsloth@icloud.com>
2016-10-22 23:14:50 -0500

    Replace individual script files with generic script file to load actor person roles

---
+++ priv/repo/seeds/initial_data/load_actor_person_roles.exs
@@ -0,0 +1,43 @@
+alias Ecto
+import Ecto.Query
+alias Cineaste.Repo
+alias Cineaste.Film
+alias Cineaste.Person
+alias Cineaste.ActorPersonRole
+
+[filename | _] = System.argv
+IO.inspect filename
+true = File.exists?(filename)
+true = File.regular?(filename)
+body = File.read!(filename)
+lines = String.split(body, "\n")
+[title | roles] = lines
+film_model = Repo.one from f in Film, where: f.title == ^title
+IO.inspect film_model
+
+by_name = fn(given_name, family_name) ->
+  person = Repo.one from p in Person, where: p.given_name == ^given_name and p.family_name == ^family_name
+  if is_nil(person) do
+    raise "#{given_name} #{family_name} not found"
+  else
+    person
+  end
+end
+
+convert_to_model = fn(role, film_model) ->
+  IO.inspect(role)
+  [given_name, family_name, order | roles] = String.split(role, ",")
+  person_model = by_name.(given_name, family_name)
+  %ActorPersonRole{
+    film_id: film_model.id,
+    person_id: person_model.id,
+    order: String.to_integer(order),
+    roles: roles
+  }
+end
+
+actor_person_roles = Enum.map(roles, fn(role) -> convert_to_model.(role, film_model) end)
+
+from(role in ActorPersonRole, where: role.film_id == ^film_model.id) |> Repo.delete_all
+
+Enum.each(actor_person_roles, fn x -> Repo.insert! x end)
