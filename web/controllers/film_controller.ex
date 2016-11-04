6f49083d5e0b4a2a65a7206b118753f1a766a9f4
James Tortorelli <onetoedsloth@icloud.com>
2016-11-04 12:20:31 -0500

    Handle bad parameters in film controller show()

--- web/controllers/film_controller.ex
+++ web/controllers/film_controller.ex
@@ -9,10 +9,30 @@ defmodule Cineaste.FilmController do
   end
 
   def show(conn, %{"id" => id}) do
-    film = Repo.get!(Film, id) |> Repo.preload([:studios])
+    _find_film(conn, Ecto.UUID.cast(id))
+  end
+  
+  defp _find_film(conn, {:ok, uuid}) do
+    _render_film_page(conn, Repo.get(Film, uuid))
+  end
+  
+  defp _find_film(conn, _) do
+    _render_film_not_found_message(conn)
+  end
+  
+  defp _render_film_page(conn, %Film{} = film) do
+    film = Repo.preload(film, [:studios])
     film_staff_views = Repo.all(from view in Cineaste.FilmStaffView, where: view.film_id == ^film.id)
     |> Enum.sort_by(fn(view) -> List.first(view.staff).order end)
     render conn, "show.html", film: film, film_staff_views: film_staff_views
+  end
+  
+  defp _render_film_page(conn, _) do
+    _render_film_not_found_message(conn)
+  end
+  
+  defp _render_film_not_found_message(conn) do
+    text conn, "Film not found" 
   end
 
 end
