6bb3f5d05d0c3319111f4ebc262c04dfc8c88d45
James Tortorelli <onetoedsloth@icloud.com>
2016-11-11 18:45:38 -0600

    Use alias in film view functions

--- web/views/film_view.ex
+++ web/views/film_view.ex
@@ -2,7 +2,7 @@ defmodule Cineaste.FilmView do
   use Cineaste.Web, :view
   import Ecto.Query
   alias Cineaste.Repo
-  alias Cineaste.Staff
+  alias Cineaste.Film
   alias Cineaste.SeriesFilm
   require Logger
   
@@ -32,11 +32,11 @@ defmodule Cineaste.FilmView do
     Timex.format!(date, "{Mfull} {D}, {YYYY}")
   end
   
-  def display_series_info(%Cineaste.Film{} = film) do
+  def display_series_info(%Film{} = film) do
     _display_series_info(Repo.preload(film, [:series])) 
   end
   
-  defp _display_series_info(%Cineaste.Film{:series => [_h|_t]} = film) do
+  defp _display_series_info(%Film{:series => [_h|_t]} = film) do
     series = List.first(film.series)
     series_film = Repo.one(from s in SeriesFilm, where: s.film_id == ^film.id and s.series_id == ^series.id)
     {:safe, precedent_template} = Repo.one(from s in SeriesFilm, where: s.series_id == ^series.id and s.order == ^(series_film.order - 1)) |> _display_series_precedent
@@ -48,7 +48,7 @@ defmodule Cineaste.FilmView do
     "" 
   end
 
-  defp _display_series_precedent(%Cineaste.SeriesFilm{} = series_film) do
+  defp _display_series_precedent(%SeriesFilm{} = series_film) do
     film = Repo.preload(series_film, [:film]).film
     render "series_precedent.html", film: film
   end
@@ -57,7 +57,7 @@ defmodule Cineaste.FilmView do
     {:safe, ""}
   end
   
-  defp _display_series_antecedent(%Cineaste.SeriesFilm{} = series_film) do
+  defp _display_series_antecedent(%SeriesFilm{} = series_film) do
     film = Repo.preload(series_film, [:film]).film
     render "series_antecedent.html", film: film
   end
