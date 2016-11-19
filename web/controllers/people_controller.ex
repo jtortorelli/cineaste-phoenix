aad24f1ed29dcb12c8c4418487a60d934f99b798
James Tortorelli <onetoedsloth@icloud.com>
2016-11-19 12:04:22 -0600

    Populate people index with showcased people

--- web/controllers/people_controller.ex
+++ web/controllers/people_controller.ex
@@ -1,8 +1,11 @@
 defmodule Cineaste.PeopleController do
   use Cineaste.Web, :controller
+  alias Cineaste.PeopleIndexView
+
 
   def index(conn, _params) do
-    render conn, "index.html"
+    people_index_views = Repo.all(PeopleIndexView) |> Enum.sort_by(fn(view) -> view.sort_name end)
+    render conn, "index.html", people_index_views: people_index_views
   end
 
   def show_person(conn, %{"id" => id}) do
--- web/views/people_view.ex
+++ web/views/people_view.ex
@@ -1,3 +1,13 @@
 defmodule Cineaste.PeopleView do
   use Cineaste.Web, :view
+
+  def render_link(conn, view) do
+    case view.type do
+      "person" ->
+        link "#{view.display_name}", to: people_path(conn, :show_person, view.id)
+      "group" ->
+        link "#{view.display_name}", to: people_path(conn, :show_group, view.id)
+    end 
+  end
+
 end
