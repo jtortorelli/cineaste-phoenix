cf5c2a8846befa17d87d565da53eb210b463665e
James Louis Tortorelli <jtortorelli03@icloud.com>
2017-01-07 20:36:32 -0600

    Render links to showcased people and groups from film page

--- web/views/film_view.ex
+++ web/views/film_view.ex
@@ -8,23 +8,23 @@ defmodule Cineaste.FilmView do
   alias Cineaste.FilmImage
   alias Cineaste.CommonView
   require Logger
-  
+
   def sorted_staff(staff), do: Enum.sort_by(staff, fn(x) -> x.order end)
-  
+
   def display_original_title(%{"original_title" => title, "original_transliteration" => transliteration, "original_translation" => translation} = props) do
     Logger.debug "inside display_original_title"
     Logger.debug "value of props: #{inspect(props)}"
     render "original_title.html", title: title, transliteration: transliteration, translation: translation
   end
-  
-  def display_original_title(_), do: nil 
-  
+
+  def display_original_title(_), do: nil
+
   def display_aliases([_head | _tail] = aliases), do: CommonView.render_aliases_table_row(aliases)
   def display_aliases(_), do: nil
-  
+
   def display_release_date(date), do: Timex.format!(date, "{Mfull} {D}, {YYYY}")
-  
-  def display_series_info(conn, %Film{} = film), do: _display_series_info(conn, Repo.preload(film, [:series]))  
+
+  def display_series_info(conn, %Film{} = film), do: _display_series_info(conn, Repo.preload(film, [:series]))
   defp _display_series_info(conn, %Film{:series => [_h|_t]} = film) do
     series = List.first(film.series)
     series_film = Repo.one(from s in SeriesFilm, where: s.film_id == ^film.id and s.series_id == ^series.id)
@@ -32,8 +32,8 @@ defmodule Cineaste.FilmView do
     {:safe, subsequent_template} = Repo.one(from s in SeriesFilm, where: s.series_id == ^series.id and s.order == ^(series_film.order + 1)) |> _display_series_subsequent(conn)
     raw "#{antecedent_template}#{subsequent_template}"
   end
-  
-  defp _display_series_info(_, _), do: nil 
+
+  defp _display_series_info(_, _), do: nil
 
   defp _display_series_antecedent(%SeriesFilm{} = series_film, conn) do
     film = Repo.preload(series_film, [:film]).film
@@ -43,9 +43,9 @@ defmodule Cineaste.FilmView do
       render "series_antecedent.html", film: film
     end
   end
-  
+
   defp _display_series_antecedent(_, _), do: {:safe, ""}
-  
+
   defp _display_series_subsequent(%SeriesFilm{} = series_film, conn) do
     film = Repo.preload(series_film, [:film]).film
     if film.showcase do
@@ -54,21 +54,21 @@ defmodule Cineaste.FilmView do
       render "series_subsequent.html", film: film
     end
   end
-  
+
   defp _display_series_subsequent(_, _), do: {:safe, ""}
-  
-  def render_top_billed_cast([_h|_t] = top_billed_cast) do
-    render "top_billed_cast.html", top_billed_cast: top_billed_cast 
+
+  def render_top_billed_cast(conn, [_h|_t] = top_billed_cast) do
+    render "top_billed_cast.html", conn: conn, top_billed_cast: top_billed_cast
   end
-  
-  def render_top_billed_cast(_), do: nil
-  
-  def render_other_cast([_h|_t] = other_cast) do
-    render "other_cast.html", other_cast: other_cast
+
+  def render_top_billed_cast(_, _), do: nil
+
+  def render_other_cast(conn, [_h|_t] = other_cast) do
+    render "other_cast.html", conn: conn, other_cast: other_cast
   end
-  
-  def render_other_cast(_), do: nil
-  
+
+  def render_other_cast(_, _), do: nil
+
   def render_gallery(film_id) do
     s3_gallery_url = Application.get_env(:cineaste, :s3)[:base_url] <> Application.get_env(:cineaste, :s3)[:film_galleries]
     file_names = Repo.all(from image in FilmImage, where: image.film_id == ^film_id and image.type == "gallery", order_by: image.file_name)
@@ -77,10 +77,22 @@ defmodule Cineaste.FilmView do
     thumb_url = s3_gallery_url <> film_id <> "/thumbs/"
     render "gallery.html", film_id: film_id, file_names: file_names, full_url: full_url, thumb_url: thumb_url
   end
-  
+
   def render_poster(film_id) do
     s3_poster_url = Application.get_env(:cineaste, :s3)[:base_url] <> Application.get_env(:cineaste, :s3)[:posters]
-    s3_poster_url <> film_id <> ".jpg" 
+    s3_poster_url <> film_id <> ".jpg"
+  end
+
+  def render_people_link(conn, id, text, true, "person") do
+    link "#{text}", to: people_path(conn, :show_person, id)
+  end
+
+  def render_people_link(conn, id, text, true, "group") do
+    link "#{text}", to: people_path(conn, :show_group, id)
+  end
+
+  def render_people_link(_, _, text, false, _) do
+    text
   end
 
 end
