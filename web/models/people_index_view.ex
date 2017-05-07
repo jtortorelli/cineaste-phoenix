da8ef6882b2a0c941ea5a4260f0a4377e60f9ec2
James Tortorelli <james.tortorelli@metova.com>
2017-05-07 13:49:21 -0500

    Add roles field to people index view model

--- web/models/people_index_view.ex
+++ web/models/people_index_view.ex
@@ -1,17 +1,18 @@
 defmodule Cineaste.PeopleIndexView do
   use Cineaste.Web, :model
-  
+
   @primary_key false
   schema "people_index_view" do
     field :id, Ecto.UUID
     field :type, :string
     field :sort_name, :string
     field :display_name, :string
+    field :roles, {:array, :string}
   end
-  
+
   def changeset(people_index_view, params \\ %{}) do
     people_index_view
-    |> cast(params, [:id, :type, :sort_name, :display_name])
-    |> validate_required([:id, :type, :sort_name, :display_name]) 
-  end 
+    |> cast(params, [:id, :type, :sort_name, :display_name, :roles])
+    |> validate_required([:id, :type, :sort_name, :display_name, :roles]) 
+  end
 end
