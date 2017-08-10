ed99fda18dfb1be97e7127e44a0222469b56ef97
James Tortorelli <james.tortorelli@metova.com>
2017-08-10 10:01:32 -0500

    update timex library (fix off-by-one errors on calculated ages on person bios)

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Cineaste.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:timex, "~> 3.0"},
+     {:timex, "~> 3.1.24"},
      {:poison, "~> 3.0", override: true},
      {:earmark, "~>1.2.0"},
      {:httpoison, "~>0.11.2"}]
